21fa858f03496d0da677762f86308353137180a8
thiry <thiry@five-seven.net>
2014-08-16 02:28:51 +0900

    Excelのデータエリアの行、列の数に応じたジェネレータを実装した

--- lib/Excel2Sql/SqlBuilder.php
+++ lib/Excel2Sql/SqlBuilder.php
@@ -4,7 +4,7 @@
      *
      * @package Excel2Sql
      * @author Thiry
-     * @since PHP 5.4
+     * @since PHP 5.5
      * @version 1.0.0
      * @uses Excel2Sql\ExcelReader
      * @uses Excel2Sql\E2SException
@@ -19,19 +19,25 @@
         * エクセルデータのプロパティ
         *
         * @property PHPExcel $sheet アクティブなシートを格納する
-        * @property int シートの行数を格納する
-        * @property int シートの列数を格納する
+        * @property int $rowCount シートの行数を格納する
+        * @property int $colCount シートの列数を格納する
+        * @property String $sqlTemp SQLテンプレートを格納する
         */
         private $sheet;
         private $rowCount = 0;
         private $colCount = 0;
+        private $sqlTemp = null;
 
         /**
         * エクセルデータのデータ位置
         *
         * @property-read String CELL_SQL SQLテンプレートが格納されているセル
+        * @property-read String CELL_DATA_START_ROW データが格納されている行の始端
+        * @property-read String CELL_DATA_START_COL データが格納されている列の始端( 0 = A )
         */
         const CELL_SQL = 'B1';
+        const CELL_DATA_START_ROW = '4';
+        const CELL_DATA_START_COL = '0';
 
         /**
          * ExcelファイルからSQLを作成する
@@ -43,8 +49,7 @@
         {
             $sb = new self(ExcelReader::open($filePath));
 
-            //シートからSQLのテンプレートを取得
-            $sqlTemp = $sb->getSqlPlaceHolder();
+            $queries = $sb->build();
 
         }
 
@@ -68,22 +73,63 @@
             //シートの列数を取得
             $this->colCount = \PHPExcel_Cell::columnIndexFromString($this->sheet->getHighestColumn());
 
+            //シートからSQLのテンプレートを取得
+            $this->setSqlPlaceHolder();
         }
 
         /**
          * ExcelからSQLテンプレートを取り出す
          */
-        private function getSqlPlaceHolder()
+        private function setSqlPlaceHolder()
         {
-            $sql = $this->sheet->getCell( $this::CELL_SQL )->getValue();
+            $this->sqlTemp = $this->sheet->getCell( $this::CELL_SQL )->getValue();
 
             //SQLの取り出しに失敗していれば例外を出す
-            if( null === $sql )
+            if( null === $this->sqlTemp )
             {
                 throw new E2SException('couldn\'t get SQL Template. column: ' . $this::CELL_SQL);
             }
 
-            return $sql;
+        }
+
+        /**
+         * Excelからデータを抽出し、SQLテンプレートと合成する
+         *
+         * @return array
+         */
+        private function build()
+        {
+
+        }
+
+        /**
+         * Excelのデータエリアの行番号を返すジェネレータ
+         * @return int 行番号
+         */
+        private function row()
+        {
+            //データの行数を取得
+            $dataRowCount = $this->rowCount - ( $this::CELL_DATA_START_ROW - 1 );
+
+            foreach( range(0, ( $dataRowCount - 1 ) ) as $rowNum )
+            {
+                yield ( $rowNum + $this::CELL_DATA_START_ROW );
+            }
+        }
+
+        /**
+         * Excelのデータエリアの列番号を返すジェネレータ
+         * @return int 列番号
+         */
+        private function col()
+        {
+            //SQL内の置き換え対象('?')の数を取得する
+            $paramCount = substr_count($this->sqlTemp, '?');
+
+            foreach( range(0, ( $paramCount - 1 ) ) as $colNum )
+            {
+                yield $colNum;
+            }
         }
 
     }
